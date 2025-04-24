c054281dd78909bbd7c263dc7fb2d6b0949b9875
Will Holland <will@MacBook-Pro-148.local>
2025-04-24 00:36:00 -0400

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_datastar_demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
